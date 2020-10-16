612f102814a1d76211b774df546d13117a6abb47
namnguyenni <namnguyen.lion@gmail.com>
2020-10-16 10:01:47 +0700

    thêm trang chi tiết lhs, trnag hoạt động và chi tiết hoạt động

---
+++ QLDHS/Controllers/HoatdongController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLDHS.Controllers
+{
+    public class HoatdongController : Controller
+    {
+        // GET: Hoatdong
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public bool AddArticle()
+        {
+            return true;
+        }
+
+        [HttpGet]
+        public ActionResult ArticleDetail(string IdArticle)
+        {
+            return View();
+        }
+    }
+}
