cf5579b9aa99b24b3abf2214b4b7a2f8669da902
khanhtoan98mta <khanhtoan99gvc@gmail.com>
2020-10-20 20:51:47 +0700

    commiy

---
+++ QLDHS/Models/Entity/inforLHS.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLDHS.Models.Entity
+{
+    public class InforLHS
+    {
+
+        public string Hoten { get; set; }
+        public string MaLHS { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public string ThongTinLienLac { get; set; }
+        public string QueQuan { get; set; }
+        public string GioiTinh { get; set; }
+        public string SoHieuSiQuan { get; set; }
+        public DateTime? NgayNhapNgu { get; set; }
+        public DateTime? NgayVaoDoan { get; set; }
+        public DateTime? NgayVaoDang { get; set; }
+        public string DanToc { get; set; }
+        public string TonGiao { get; set; }
+        public string Mien { get; set; }
+        public double? HocPhi { get; set; }
+        public double? SinhHoatPhi { get; set; }
+        public double? BHYT { get; set; }
+        public double? ChiPhiKhac { get; set; }
+        public string GiaHanThoiGianDaoTao { get; set; }
+        public string Image { get; set; }
+        public string QuanHam { get; set; }
+        public string NghienCuuNoiBat { get; set; }
+        public int? MaDVBM { get; set; }
+        public int? MaDVBQP { get; set; }
+        public string CSDaoTao { get; set; }
+        public string DiaBan { get; set; }
+        public int? MaKetQua { get; set; }
+        public double? Ki1 { get; set; }
+        public double? Ki2 { get; set; }
+        public double? Ki3 { get; set; }
+        public double? Ki4 { get; set; }
+        public double? Ki5 { get; set; }
+        public double? Ki6 { get; set; }
+        public double? Ki7 { get; set; }
+        public double? Ki8 { get; set; }
+        public double? Ki9 { get; set; }
+        public double? Ki10 { get; set; }
+        public double? Ki11 { get; set; }
+        public double? Ki12 { get; set; }
+        public double? Ki13 { get; set; }
+        public double? Ki14 { get; set; }
+        public string PhanLoaiTotNghiep { get; set; }
+        public double? DiemTrungBinh { get; set; }
+        public string MoTaKetQua { get; set; }
+        public string LuuNoMon { get; set; }
+        public int? LHSID { get; set; }
+        public string MaCNDaotao1 { get; set; }
+        public string MaCNDaotao2 { get; set; }
+        public string BoMon { get; set; }
+        public int? MaDVCapTren { get; set; }
+        public string Khoa { get; set; }
+        public string DonViBQP { get; set; }
+        public string CNDT1 { get; set; }
+        public int? MN1 { get; set; }
+        public string CNDT2 { get; set; }
+        public int? MN2 { get; set; }
+        public string QuaTrinhCongTac { get; set; }
+        public string TenLuanVan { get; set; }
+        public string KetQuaBaoVe { get; set; }
+        public string QuyetDinhDuHoc { get; set; }
+        public string NganhDT1 { get; set; }
+        public string NganhDT2 { get; set; }
+        public string KiLuat { get; set; }
+        public string KhenThuong { get; set; }
+        public string CanBo { get; set; }
+        public string DiaChiLienLac { get; set; }
+        public string ThongTinGiaDinh { get; set; }
+        public string NoiOHienNay { get; set; }
+        public string ThanhPhanGiaDinh { get; set; }
+
+    }
+}
